916539c1231629794d011fad80d4ec0613dc65c1
selenajiro <selena.jiro@gmail.com>
2026-09-13 13:52:03 +0300

    Add RestClient dependency, enable scheduling, configure import HTTP timeouts

--- src/main/java/io/github/selenajiro/europolitik/EuroPolitikApplication.java
+++ src/main/java/io/github/selenajiro/europolitik/EuroPolitikApplication.java
@@ -2,8 +2,10 @@ package io.github.selenajiro.europolitik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EuroPolitikApplication {
 
     public static void main(String[] args) {
